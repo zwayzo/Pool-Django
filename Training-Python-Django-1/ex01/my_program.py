f7d6204f9a1e7c1b68edb4a7c27736e33492cbc9
zwayzo <medazzeddine48@gmail.com>
2025-09-27 22:52:27 +0100

    f

--- Training-Python-Django-1/ex01/my_program.py
+++ Training-Python-Django-1/ex01/my_program.py
@@ -1,8 +1,16 @@
-from path import Path
+#  It must create a folder and a file inside this folder,
+# write something in this file and
+# then read and display its conte
 
-# Create a Path object for current folder
-p = Path(".")
+from local_lib.path import Path
 
-# Print all files in the folder
-for f in p.files():
-    print(f)
+# Create a Path object for a file inside a folder
+file_path = Path("test_folder/test_file.txt")
+
+# Create necessary directories
+file_path.parent.mkdir_p()
+
+# Write content to the file
+file_path.write_text("This is the test data.")
+
+print(f"File '{file_path}' created successfully.")
